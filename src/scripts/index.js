d5f20b9a127ba750a0cf175133e2b21ef3248c42
Dewa Palguna <dewapalguna900@gmail.com>
2021-05-29 19:28:48 +0800

    Complete Drawer Function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,10 @@ import 'regenerator-runtime';
 import '../styles/main.css';
 import '../styles/responsive.css';
 
-console.log('Hello Coders!');
+import App from './views/app';
+
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
