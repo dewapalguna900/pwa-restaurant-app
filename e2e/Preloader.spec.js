719fc49e792337f958aadf7cee850d8ae0471487
Dewa Palguna <dewapalguna900@gmail.com>
2021-06-21 19:42:55 +0800

    Compete Preloader E2E Testing

---
+++ e2e/Preloader.spec.js
@@ -0,0 +1,24 @@
+Feature('Preloader');
+
+Scenario('Preloader activated on homepage', async ({ I }) => {
+  I.amOnPage('/');
+
+  I.seeElement('#preloader__wrapper');
+
+  if (I.seeElement('.restaurant-item')) {
+    I.dontSeeElement('#preloader__wrapper');
+  }
+});
+
+Scenario('Preloader activated on detail page', async ({ I }) => {
+  I.amOnPage('/');
+  I.seeElement('.restaurant__list__title a');
+
+  I.click(locate('.restaurant__list__title a').first());
+
+  I.seeElement('#preloader__wrapper');
+
+  if (I.seeElement('.restaurant__title')) {
+    I.dontSeeElement('#preloader__wrapper');
+  }
+});
